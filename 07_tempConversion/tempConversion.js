b2f486b294cdc427b1fde887b965225b55f32623
John <144583586+Questiomaconmark@users.noreply.github.com>
2024-10-07 23:50:53 -0600

    Completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+  fahrTemp = (fahrTemp - 32) * (5/9);
+  fahrTemp = Math.round(fahrTemp * 10) / 10;
+  return fahrTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  celTemp = (celTemp * (9/5)) + 32;
+  celTemp = Math.round(celTemp * 10) / 10;
+  return celTemp;
 };
 
 // Do not edit below this line
